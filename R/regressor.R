51bb5f87e7e5d24e5d48458e4ce277a6674c5ff9
Ian Johnson <ianjjohnson@icloud.com>
2017-03-08 12:24:22 -0600

    update regressor to allow discretization parameters

--- R/regressor.R
+++ R/regressor.R
@@ -1,5 +1,6 @@
 bCBA.regression <- function(formula, data, support = 0.1, confidence = 0.8, gamma = 0.02, cost = 1, num_levels = 20,
-                           verbose=FALSE, parameter = NULL, control = NULL, sort.parameter = NULL, lhs.support=TRUE){
+                           verbose=FALSE, parameter = NULL, control = NULL, sort.parameter = NULL, lhs.support=TRUE,
+													 disc.method="cluster", disc.categories=10){
 
   formula <- as.formula(formula)
   class <- as.character(formula[[2]])
@@ -10,7 +11,8 @@ bCBA.regression <- function(formula, data, support = 0.1, confidence = 0.8, gamm
   data[[class]] <- discretize(data[[class]], categories = num_levels)
 
   clf <- bCBA(formula, data, support=support, confidence=confidence, gamma=gamma, cost=cost,
-              verbose=verbose, parameter=parameter, control=control, sort.parameter=sort.parameter, lhs.support=lhs.support)
+              verbose=verbose, parameter=parameter, control=control, sort.parameter=sort.parameter,
+							lhs.support=lhs.support, disc.method=disc.method, disc.categories=disc.categories)
   clf$means <- unlist(lapply(strsplit(levels(data[[clf$class]]), ','), function(x) (as.numeric(substr(x[1], 2, nchar(x[1]))) +  as.numeric(substr(x[2], 1, nchar(x[2]) - 1))) /2 ))
   clf$mean <- mean
   clf$method <- "weightedmean"
